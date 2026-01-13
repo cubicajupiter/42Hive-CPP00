0b3cd2a448c5b00780fb035f9e3795ef4b6bbe2c
Janne Valkama <jvalkama@c1r3p5.hive.fi>
2026-01-13 18:12:18 +0200

    accounts draft finished

--- ex02/src/Account.cpp
+++ ex02/src/Account.cpp
@@ -1,58 +1,101 @@
 #include "Account.hpp"
-    
-static int  Account::getNbAccounts( void ) {
-    ;
+#include <iostream>
+
+//INFORMATION DISPLAY FUNCTIONS
+int Account::getNbAccounts( void ) {
+    return _nbAccounts;
 }
 
-static int	Account::getTotalAmount( void ) {
-    ;
+int	Account::getTotalAmount( void ) {
+    return _totalAmount;
 }
 
-static int	Account::getNbDeposits( void ) {
-    ;
+int	Account::getNbDeposits( void ) {
+    return _totalNbDeposits;
 }
 
-static int	Account::getNbWithdrawals( void ) {
-    ;
+int	Account::getNbWithdrawals( void ) {
+    return _totalNbWithdrawals;
 }
 
-static void Account::displayAccountsInfos( void ) {
-    _nbAccounts;
-    _totalAmount;
-    _totalNbDeposits;
-    _totalNbWithdrawals;
+void    Account::_displayTimestamp( void ) {
+    time_t      time_now;
+    struct tm   timestruct;
+    char        buf[80];
+
+    timestruct = *localtime(&time_now);
+    strftime(buf, sizeof(buf), "[%Y%m%d_%H%M%S] ", &timestruct);
+    std::cout << buf;
 }
+
+void Account::displayAccountsInfos( void ) {
+    _displayTimestamp();
+    std::cout << "accounts:" + std::to_string(getNbAccounts()) << ";"
+    << "total:" + std::to_string(getTotalAmount()) << ";"
+    << "deposits:" + std::to_string(getNbDeposits()) << ";"
+    << "withdrawals:" + std::to_string(getNbWithdrawals()) << std::endl;
+}
+
+void    Account::displayStatus( void ) const {
+    _displayTimestamp();
+    std::cout << "index:" + std::to_string(this->_accountIndex) << ";"
+    << "amount:" + std::to_string(this->_amount) << ";"
+    << "deposits:" + std::to_string(this->_nbDeposits) << ";"
+    << "withdrawals:" + std::to_string(this->_nbWithdrawals) << std::endl;
+}
+
 
 //CONSTRUCTORS
 Account::Account( void ) {
-    ;
+    this->_accountIndex += 1; //index probably screwed
+	this->_amount = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
 }
 Account::Account( int initial_deposit ) {
-    ;
+    this->_accountIndex += 1; //index probably screwed
+    this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;;
 }
+
 
 //DESTRUCTOR
-~Account( void ) {
-    ;
+Account::~Account( void ) {
+    std::cout << "index:" + std::to_string(this->_accountIndex) << ";"
+    << "amount:" + std::to_string(this->_amount) << ";"
+    << "closed" << std::endl;
 }
 
-void	Account::makeDeposit( int deposit ) {
-    ;
+
+//ACCOUNT MANIPULATION FUNCTIONS
+void    Account::makeDeposit( int deposit ) {
+    std::cout << "index:" + std::to_string(this->_accountIndex) << ";"
+    << "p_amount:" + std::to_string(this->_amount) << ";"
+    << "deposit:" + std::to_string(deposit) << ";";
+    this->_amount += deposit;
+    this->_nbDeposits++;
+    std::cout << "amount" + std::to_string(this->_amount)
+    << "nb_deposit" + std::to_string(this->_nbDeposits) << std::endl;
 }
 
-bool	Account::makeWithdrawal( int withdrawal ) {
-    ;
+bool    Account::makeWithdrawal( int withdrawal ) {
+    std::cout << "index:" + std::to_string(this->_accountIndex) << ";"
+    << "p_amount:" + std::to_string(this->_amount) << ";"
+    << "withdrawal:";
+    if (checkAmount() < withdrawal) {
+        std::cout << "refused" << std::endl;
+        return false;
+    }
+    else {
+        this->_amount -= withdrawal;
+        std::cout << withdrawal;
+    }
+    std::cout << "amount:" + std::to_string(this->_amount) << ";"
+    << "nb_withdrawals:" + std::to_string(this->_nbWithdrawals) << std::endl;
+    return true;
 }
 
-int		Account::checkAmount( void ) const {
-    ;
-}
-
-void	Account::displayStatus( void ) const {
-    ;
-}
-
-//PRIVATE FUNCTION
-static void	Account::_displayTimestamp( void ) {
-    ;
+int Account::checkAmount( void ) const {
+    return this->_amount;
 }
